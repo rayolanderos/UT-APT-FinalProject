37c49ba40d53fdca886a9fbdcba8b7540323ca51
rayolanderos <rayolanderos@gmail.com>
2017-11-13 21:02:38 -0600

    Get single beer
    Gets single beer info and returns json

--- webapp/services/get_beer.py
+++ webapp/services/get_beer.py
@@ -7,7 +7,31 @@ from models.beer import Beer
 
 class GetBeer(webapp2.RequestHandler):
 
-    def post(self):
+    def get(self):
 
-        json_string = self.request.body
+		beer_id = int( self.request.get('beerId', 0) )
+
+		beer = Beer.get_by_id(beer_id)
+
+		if beer != None:
+			beer_data = { 
+				"beerId" : beer.key.id(),
+			    "beerName" : beer.name, 
+			    "beerStyle" : beer.style,
+			    "beerTapListImage" : beer.tap_list_image,
+			    "beerDescription" : beer.description,
+			    "beerDescriptionImage" : beer.description_image,
+			    "beerPrice" : beer.price,
+			    "beerAbv" : beer.abv,
+			    "beerIbus" : beer.ibus,
+			    "beerSrm" : beer.srm,
+			    "beerOnTap" : beer.on_tap
+			}
+		else:
+			beer_data = {}
+
+		self.response.out.write(json.dumps(beer_data))
+		
+
+
         
