34cd6336b01a526786c2ee292a1ce9c4d6887d67
Memo Leon <gleonoliva@users.noreply.github.com>
2017-11-28 00:00:28 -0600

    Implemented order from history
    I had to refactor all the payment code in order to make it more understandable, also should be easier to abstract to a base clase if we later want to implement other payment methods.

--- androidapp/HopSquadApp/app/src/main/java/com/hopsquad/hopsquadapp/fragments/ConfirmOrderFragment.java
+++ androidapp/HopSquadApp/app/src/main/java/com/hopsquad/hopsquadapp/fragments/ConfirmOrderFragment.java
@@ -2,34 +2,46 @@ package com.hopsquad.hopsquadapp.fragments;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.arch.lifecycle.ViewModelProviders;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
 import com.hopsquad.hopsquadapp.R;
-import com.hopsquad.hopsquadapp.activities.MainActivity;
-import com.hopsquad.hopsquadapp.models.Order;
-import com.hopsquad.hopsquadapp.viewmodels.TapListViewModel;
 
 import java.text.NumberFormat;
 
-public class ConfirmOrderFragment extends DialogFragment {
+public class ConfirmOrderFragment extends BaseDialogFragment {
+
+    private static final String TOTAL_PRICE_TO_PAY = "TOTAL_PRICE";
+    private OnConfirmDialogOptionSelectedListener mListener;
+    private float mTotal;
+
+    public interface OnConfirmDialogOptionSelectedListener {
+
+        void onConfirmDialogOptionSelected(boolean confirmed);
+    }
+
+    public static ConfirmOrderFragment newInstance(float total) {
+        ConfirmOrderFragment fragment = new ConfirmOrderFragment();
+        Bundle args = new Bundle();
+        args.putFloat(TOTAL_PRICE_TO_PAY, total);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     public ConfirmOrderFragment() {
         // Required empty public constructor
     }
 
-    TapListViewModel viewModel;
+    public void setOnConfirmDialogOptionSelectedListener(OnConfirmDialogOptionSelectedListener listener) {
+        this.mListener = listener;
+    }
 
     @Override
     public void onCreate(Bundle savedInstaceState) {
         super.onCreate(savedInstaceState);
-
-        viewModel = ViewModelProviders.of(getTapListFragment()).get(TapListViewModel.class);
+        mTotal = getArguments().getFloat(TOTAL_PRICE_TO_PAY);
     }
 
     @Override
@@ -39,42 +51,17 @@ public class ConfirmOrderFragment extends DialogFragment {
         View dialogView = inflater.inflate(R.layout.fragment_confirm_order, null);
 
         TextView totalTextView = dialogView.findViewById(R.id.totalConfirmationDialogText);
-        totalTextView.setText(NumberFormat.getCurrencyInstance().format(viewModel.getOrderTotal()));
+        totalTextView.setText(NumberFormat.getCurrencyInstance().format(mTotal));
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setView(dialogView)
                 .setTitle(R.string.confirm_order_dialog_title)
-                .setPositiveButton(R.string.confirm_order_dialog_button, new DialogClickHandler(this, UserAction.ACCEPTED))
-                .setNegativeButton(R.string.cancel_order_button_dialog, new DialogClickHandler(this, UserAction.CANCELED));
+                .setPositiveButton(R.string.confirm_order_dialog_button, (dialogInterface, i) -> {
+                    mListener.onConfirmDialogOptionSelected(true);
+                })
+                .setNegativeButton(R.string.cancel_order_button_dialog, (dialogInterface, i) -> {
+                    mListener.onConfirmDialogOptionSelected(false);
+                });
         return builder.create();
-    }
-
-    private TapListFragment getTapListFragment() {
-        return (TapListFragment) getFragmentManager().findFragmentByTag(MainActivity.TAP_LIST_FRAGMENT_TAG);
-    }
-
-    private enum UserAction { CANCELED, ACCEPTED };
-
-    private static class DialogClickHandler implements DialogInterface.OnClickListener {
-        private UserAction action;
-        private ConfirmOrderFragment fragment;
-
-        public DialogClickHandler(ConfirmOrderFragment fragment, UserAction action) {
-            this.fragment = fragment;
-            this.action = action;
-        }
-
-        private boolean wasCanceled() {
-            return action == UserAction.CANCELED;
-        }
-
-        @Override
-        public void onClick(DialogInterface dialogInterface, int i) {
-            if (wasCanceled()) {
-                dialogInterface.cancel();
-            } else {
-                fragment.getTapListFragment().confirmOrder();
-            }
-        }
     }
 }
