d4da71a6a092da281761c124e313009535e30a4b
rayolanderos <rayolanderos@gmail.com>
2017-11-27 20:28:55 -0600

    Indentation, google index, and class detection error
    - Fixed indentation issues in add_order, get_all_orders, and get_all_orders_by_user
    - Changed order display to customer_name vs user_id
    - Added search index for getting single user orders
    - Fixed class misname issue in the api registration

--- webapp/services/get_all_orders_by_user.py
+++ webapp/services/get_all_orders_by_user.py
@@ -9,18 +9,18 @@ from models.beer import Beer
 
 class GetAllOrdersByUser(webapp2.RequestHandler):
 
-    def get(self):
+	def get(self):
 
 		self.response.headers['Content-Type'] = 'application/json'
 
-        user_id = self.request.get('userId', 0)
-        limit = int(self.request.get('limit', 3))
+		user_id = self.request.get('userId', 0)
+		limit = int(self.request.get('limit', 3))
 
 		order_query = Order.query(Order.fb_user_id == user_id).order(-Order.timestamp)
 		orders = order_query.fetch(limit)
 		order_list = []
 
-        for order in orders:
+		for order in orders:
 			date = order.timestamp
 			date_string = date.strftime('%m/%d/%Y %H:%M:%S')
 
@@ -44,15 +44,15 @@ class GetAllOrdersByUser(webapp2.RequestHandler):
 				})
 
 			order_list.append({
-		    	'id': order.key.id(), 
-		    	'order_total': order.order_total, 
-		    	'user_id': order.fb_user_id,
-		    	'status': order.status, 
-		    	'reward_id': order.reward_id, 
-		    	'discount': order.discount, 
-		    	'invoice_number': order.invoice_number, 
-		    	'timestamp' : date_string, 
-		    	'details' : order_beer_list
-		    })
+				'id': order.key.id(), 
+				'order_total': order.order_total, 
+				'user_id': order.fb_user_id,
+				'status': order.status, 
+				'reward_id': order.reward_id, 
+				'discount': order.discount, 
+				'invoice_number': order.invoice_number, 
+				'timestamp' : date_string, 
+				'details' : order_beer_list
+			})
 
 		self.response.out.write(json.dumps(order_list))
